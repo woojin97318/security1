5469c44037a404f42e93f263271021021da75521
jeonwujin <woojin97318@gmail.com>
2023-05-16 22:47:36 +0900

    feat: 스프링 시큐리티 초기 설정

---
+++ src/main/java/com/cos/security1/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.cos.security1.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity // Spring Security Filter가 스프링 필터체인에 등록이 된다.
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        // token을 사용하는 방식이기 때문에 csrf disable
+        httpSecurity.csrf().disable();
+
+        httpSecurity.authorizeRequests()
+                // 인증이 필요
+                .antMatchers("/user/**").authenticated()
+                // 인증 & 권한 필요
+                .antMatchers("/manager/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')")
+                .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+                // other request는 전부 허용 & 권한이 없는 페이지는 login페이지로 이동
+                .anyRequest().permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/login");
+
+        return httpSecurity.build();
+    }
+}
